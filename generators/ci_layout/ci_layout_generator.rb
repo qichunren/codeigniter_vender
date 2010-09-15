cb3342f3835bbcd0742ba8e91d63f353fc83d23f
Qichunren <whyruby@gmail.com>
2010-09-15 16:04:55 +0800

    add use-jquery option to ci_layout generator.

--- generators/ci_layout/ci_layout_generator.rb
+++ generators/ci_layout/ci_layout_generator.rb
@@ -1,10 +1,24 @@
 class CiLayoutGenerator < Rails::Generator::Base
+
+  default_options :use_jquery => false
   
   def manifest
     record do |m|
       m.directory "public/system/application/views/layouts"
-      m.template "application_layout.php.erb", "public/system/application/views/layouts/application_layout.php"
+      m.template "application_layout.php.erb", "public/system/application/views/layouts/application_layout.php", :assigns => { :use_jquery => options[:use_jquery] }
     end
+  end
+
+  def banner
+    "Usage: #{$0} ci_layout [options]"
+  end
+
+  protected
+  
+  def add_options!(opt)
+    opt.separator ''
+    opt.separator 'Options:'
+    opt.on("--use-jquery", "Use google jquery api") { |v| options[:use_jquery] = true }
   end
   
 end
