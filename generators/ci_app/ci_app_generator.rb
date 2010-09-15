ed37cc9e512b2e372692b957ed7b09e2b99254f1
Qichunren <whyruby@gmail.com>
2010-09-15 11:56:57 +0800

    First comment

---
+++ generators/ci_app/ci_app_generator.rb
@@ -0,0 +1,10 @@
+class CiAppGenerator < Rails::Generator::Base
+  def manifest
+    record do |m|
+      m.file "CodeIgniter_1.7.2/index.php", "public/index.php"
+      system_directory = source_path("CodeIgniter_1.7.2/system")
+      public_system_directory = destination_path("public/system")
+      system("cp -r #{system_directory} #{public_system_directory}")
+    end
+  end
+end
---
+++ lib/tasks/codeigniter_vender.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :codeigniter_vender do
+#   # Task goes here
+# end
