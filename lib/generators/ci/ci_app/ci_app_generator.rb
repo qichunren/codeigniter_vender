b2c3dce790dda40d4ca5acb15dd16a6b3e027a54
qichunren <whyruby@gmail.com>
2011-02-19 12:36:44 +0800

    move

--- lib/generators/ci/ci_app/ci_app_generator.rb
+++ lib/generators/ci/ci_app/ci_app_generator.rb
@@ -1,15 +1,6 @@
-class CiAppGenerator < Rails::Generator::Base
+class CiAppGenerator < Base
   
-  def manifest
-    record do |m|
-      m.file "CodeIgniter_1.7.2/index.php", "public/index.php"
-      system_directory = source_path("CodeIgniter_1.7.2/system")
-      public_system_directory = destination_path("public/system")
-      index_html_file = destination_path("public/index.html")
-      system("rm #{index_html_file}")
-      system("cp -r #{system_directory} #{public_system_directory}")
-      puts "Done."
-    end
-  end
-  
+  def generate_app
+    
+  end  
 end
