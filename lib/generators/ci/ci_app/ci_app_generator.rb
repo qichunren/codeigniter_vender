a97c40d315e4cc0c27dbfb141d4ece3d4b5c1f94
qichunren <whyruby@gmail.com>
2011-02-19 12:42:43 +0800

    fix a bug

--- lib/generators/ci/ci_app/ci_app_generator.rb
+++ lib/generators/ci/ci_app/ci_app_generator.rb
@@ -1,6 +1,9 @@
-class CiAppGenerator < Base
+module CodeigniterVender
+  module Generators
+    class CiAppGenerator < Base
   
-  def generate_app
-    directory "CodeIgniterLibs/CodeIgniter_2.0.0", "public/ci", :recursive => true  
-  end  
+      def generate_app
+        directory "CodeIgniterLibs/CodeIgniter_2.0.0", "public/ci", :recursive => true  
+      end  
+  end
 end
