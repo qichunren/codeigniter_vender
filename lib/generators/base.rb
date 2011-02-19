8199ce2d7b3d08adef8c8e05a2bb3ed949d96ca8
qichunren <whyruby@gmail.com>
2011-02-19 12:49:43 +0800

    fix a bug

--- lib/generators/base.rb
+++ lib/generators/base.rb
@@ -2,7 +2,7 @@ require 'rails/generators/named_base'
 
 module CodeigniterVender
   module Generators
-    class Base < Rails::Generators::NamedBase #:nodoc:
+    class CiBase < Rails::Generators::Base #:nodoc:
       def self.source_root
         @_rspec_source_root ||= File.expand_path(File.join(File.dirname(__FILE__), 'ci', generator_name, 'templates'))
       end
